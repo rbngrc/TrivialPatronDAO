5d905ef33b5f7900782063c08fdeea44564471db
Ruben Garcia <rubengarciarbngrc@gmail.com>
2021-04-05 11:48:49 +0200

    final commit adding README

---
+++ README.txt
@@ -0,0 +1,19 @@
+Trivial versión 05/04/21
+========================================================
+
+NOTAS GENERALES
+---------------
+
+Juego de preguntas tipo Trivial con varias respuestas posibles.
+
+· Añadido soporte para permitir lectura y escritura de datos tanto en ficheros como en base de datos mediante patron DAO
+
+NOTAS ESPECIFICAS
+-----------------
+
+No existen.
+
+OTRAS CONSIDERACIONES
+---------------------
+
+No se consideran.
--- src/dam2/add/p6/Menu.java
+++ src/dam2/add/p6/Menu.java
@@ -1,5 +1,8 @@
 package dam2.add.p6;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -7,15 +10,56 @@ import java.util.InputMismatchException;
 import java.util.ResourceBundle;
 import java.util.Scanner;
 
-public class Menu {
+import org.apache.log4j.FileAppender;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PatternLayout;
+import org.apache.log4j.PropertyConfigurator;
 
+import java.util.Properties;
+
+public class Menu {
+	private static Logger log = Logger.getLogger(OperacionesBaseDatos.class);
+	
 	private static Connection conexion;
 	private static Statement st;
 
 	public void Menu(ResourceBundle rb) throws SQLException {
+		
+		PropertyConfigurator.configure("./properties/log4j.properties");
+		
+		try {
+			log.addAppender(new FileAppender(new PatternLayout(),"./properties/prueba.log", false));
+		} catch (IOException e) {
+			e.printStackTrace();
+			log.error(e.toString());
+		}
+		
 		Scanner sc = new Scanner(System.in);
-		OpsDataBaseDAO operacion = new OpsDataBaseDAO();
 		Ops comunOps = new Ops();
+		Properties properties = new Properties();
+		
+		String repositorio = "./properties/persistencia.properties";
+		IJugadorDAO daoJug = null;
+		IPreguntaDAO daoPreg = null;
+		
+		try {
+			properties.load(new FileInputStream(repositorio));
+			
+			if (repositorio.equals("mysql")) {
+				daoJug = new OpsDataBaseDAO();
+				daoPreg = new OpsDataBaseDAO();
+			} else if (repositorio.equals("fichero")){
+				daoJug = new OpsFileDAO();
+				daoPreg = new OpsFileDAO();
+			}
+			
+		} catch (FileNotFoundException e) {
+			log.error(e.toString());
+		} catch (IOException e) {
+			log.error(e.toString());
+		}
+		
+		
 
 		boolean salir = false;
 		int opcion;
@@ -39,22 +83,22 @@ public class Menu {
 
 				switch (opcion) {
 				case 1:
-					operacion.jugar();
+					daoPreg.jugar();
 					st.close();
 					ConexionBaseDatos.desconectar();
 					break;
 				case 2:
-					operacion.anadirPreguntas();
+					daoPreg.anadirPreguntas();
 					st.close();
 					ConexionBaseDatos.desconectar();
 					break;
 				case 3:
-					operacion.importarPreguntas();
+					daoPreg.importarPreguntas();
 					st.close();
 					ConexionBaseDatos.desconectar();
 					break;
 				case 4:
-					operacion.verRecords();
+					daoJug.verRecords();
 					st.close();
 					ConexionBaseDatos.desconectar();
 					break;
